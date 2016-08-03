c2b56e6b80f054ecf3a0af22616b77b081ba7ee3
tbduong <tiffaniduong@gmail.com>
2016-08-03 11:26:43 -0700

    initiate routes for index and show

---
+++ scripts/controllers/MovieIndexController.js
@@ -0,0 +1,16 @@
+angular.module('imdbAndChill')
+  .controller('MovieIndexController', MovieIndexController);
+
+MovieIndexController.$inject=[];
+function MovieIndexController() {
+  console.log("MovieIndexController online!");
+  var vm = this;
+
+  // vm.sayHi = sayHi;
+//
+//   ////
+//
+//   function () {
+//     return "Hello " + name;
+//   }
+}
